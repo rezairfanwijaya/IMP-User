d57bb92b3263187d87c6fdd856a86549181e25c6
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-20 21:03:51 +0700

    add: utils hash and verify password

--- helper/helper.go
+++ helper/helper.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func GetENV(path string) (map[string]string, error) {
@@ -16,4 +17,27 @@ func GetENV(path string) (map[string]string, error) {
 	}
 
 	return env, nil
+}
+
+func HashingPassword(rawPassoword string) (string, error) {
+	passwordHashed, err := bcrypt.GenerateFromPassword([]byte(rawPassoword), 10)
+	if err != nil {
+		return "", fmt.Errorf(
+			"gagal hashing password :%v",
+			err.Error(),
+		)
+	}
+
+	return string(passwordHashed), nil
+}
+
+func VerifyPassword(hashedPassword, rawPassword string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(rawPassword)); err != nil {
+		return fmt.Errorf(
+			"gagal verifikasi password : %v",
+			err.Error(),
+		)
+	}
+
+	return nil
 }
--- helper/helper_test.go
+++ helper/helper_test.go
@@ -10,3 +10,18 @@ func TestGetENV(t *testing.T) {
 
 	t.Log(env)
 }
+
+func TestHashedPassword(t *testing.T) {
+	res, err := HashingPassword("12345")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	t.Log(res)
+}
+
+func TestVerifyPassword(t *testing.T) {
+	if err := VerifyPassword("$2a$10$5xE4X8yW1iQr5MHUGx0KU.qwdpQEImqL.8/zNWO5KpIKrTpXMcKci", "12345"); err != nil {
+		t.Fatal(err.Error())
+	}
+}
