9afb234e0412e3607f90b13ddada3e3d1e0c6a3d
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-20 23:17:37 +0700

    add: input user login

--- user/input.go
+++ user/input.go
@@ -5,3 +5,8 @@ type InputNewUser struct {
 	Password string `json:"password" binding:"required,min=5"`
 	FullName string `json:"fullname" binding:"required"`
 }
+
+type InputLoginUser struct {
+	Username string `json:"username" binding:"required,min=2"`
+	Password string `json:"password" binding:"required,min=5"`
+}
