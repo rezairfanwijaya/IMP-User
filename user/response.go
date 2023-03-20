fab19cbc76f5ce021b060bde71cf1fda7553776f
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-20 23:18:49 +0700

    add: reponse user success login

--- user/response.go
+++ user/response.go
@@ -6,10 +6,26 @@ type userResponse struct {
 	Fullname string `json:"fullname"`
 }
 
+type userReponseLogin struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Fullname string `json:"fullname"`
+	Token    string `json:"token"`
+}
+
 func FormatUser(user User) *userResponse {
 	return &userResponse{
 		ID:       user.ID,
 		Username: user.Username,
 		Fullname: user.FullName,
+	}
+}
+
+func FormatUserLogin(user User, token string) *userReponseLogin {
+	return &userReponseLogin{
+		ID:       user.ID,
+		Username: user.Username,
+		Fullname: user.FullName,
+		Token:    token,
 	}
 }
