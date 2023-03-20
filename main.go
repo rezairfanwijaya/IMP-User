99e30f4b3fc9d200e2a7f0af10e84a0b23802384
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-03-20 22:39:39 +0700

    add: endpoint user signup

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "log"
+import (
+	"imp/database"
+	"imp/handler"
+	"imp/helper"
+	"imp/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	log.Println("Hello World")
+	// connection to db
+	conn, err := database.NewConnection(".env")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	router := gin.Default()
+
+	userRepo := user.NewRepository(conn)
+	userService := user.NewService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	// endpoint
+	router.POST("auth/signup", userHandler.SignUp)
+
+	env, err := helper.GetENV(".env")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if err := router.Run(env["DOMAIN"]); err != nil {
+		log.Fatal(err.Error())
+	}
 }
